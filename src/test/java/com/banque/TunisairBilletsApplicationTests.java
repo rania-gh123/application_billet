8ce5ab49e55782815b0e67c8061460c288c19f1c
rania-gh123 <raniagharbi718@gmail.com>
2026-04-16 16:58:43 +0200

    fist comit

---
+++ src/test/java/com/banque/TunisairBilletsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunisairBilletsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
